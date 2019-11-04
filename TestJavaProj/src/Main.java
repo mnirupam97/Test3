2411a8fd02ad2ce5fdaf727a1387325feb067886
nirupam mahindra <mahindranirupam@gmail.com>
2019-11-03 22:13:04 -0800

    Java Project added

---
+++ TestJavaProj/src/Main.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello world!!!!!!!!!!");
+	}
+
+}
